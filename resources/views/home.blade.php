c32f32521b6affa46636d627c8022fb082ba59b3
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-26 15:17:57 +0100

    setup layout main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h3>Home here</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel aut quas praesentium incidunt. Reprehenderit odit ipsum cum soluta asperiores excepturi facilis, blanditiis velit tempora aut, enim animi, saepe tempore minima.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel aut quas praesentium incidunt. Reprehenderit odit ipsum cum soluta asperiores excepturi facilis, blanditiis velit tempora aut, enim animi, saepe tempore minima</p>
 @endsection
