3270f6ce3a493009cdb5307b90d7773aaa93aec3
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-26 16:06:46 +0100

    print data in db

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\comic;
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comic-data');
+
+       foreach ($comics as $comic) {
+           $new_comic = new comic();
+           $new_comic->title = $comic['title'];
+           $new_comic->description = $comic['description'];
+           $new_comic->thumb = $comic['thumb'];
+           $new_comic->price = $comic['price'];
+           $new_comic->series = $comic['series'];
+           $new_comic->sale_date = $comic['sale_date'];
+           $new_comic->type = $comic['type'];
+
+           $new_comic->save();
+       }
+    }
+}
