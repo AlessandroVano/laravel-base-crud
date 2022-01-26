2b7f520b1826ede7b588552066edf8ab87f94cf2
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-26 16:53:42 +0100

    create table

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -16,7 +16,7 @@ class ComicController extends Controller
         $comics = comic::all();
         dump($comics);
 
-        return view('comics.index');
+        return view('comics.index', compact('comics'));
     }
 
     /**
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,35 @@
 @section('content')
     <section class="container">
         <h1>COMICS</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Thumb</th>
+                    <th>price</th>
+                    <th>Series</th>
+                    <th>Sale_date</th>
+                    <th>Type</th>
+                    
+                </tr>
+                <tbody>
+                      @foreach ( $comics as $comic )
+                          <tr>
+                              <td> {{$comic->id}} </td>
+                              <td> {{$comic->title}} </td>
+                              <td class="text-truncate" style="max-width: 350px"> {{$comic->description}} </td>
+                              <td class="text-truncate" style="max-width: 350px"> {{$comic->thumb}} </td>
+                              <td> {{$comic->price}} </td>
+                              <td> {{$comic->series}} </td>
+                              <td> {{$comic->sale_date}} </td>
+                              <td> {{$comic->type}} </td>
+                          </tr>
+                      @endforeach
+                </tbody>
+            </thead>
+        </table>
     </section>
 @endsection
